b0383eab4abc5d685b597a78df812a6f232d4947
samzana <samuelzana18@gmail.com>
2025-04-09 10:05:28 +0200

    fix build issue

---
+++ app/aitools/bestpractices/page.tsx
@@ -0,0 +1,14 @@
+import Navbar from "@/app/components/Navbar"
+
+const BestPractices = () => {
+    return (
+        <>
+        <Navbar />
+        <main style={{ maxWidth: '1000px', margin: '20px auto', padding: '20px', fontFamily: 'Arial, sans-serif', textAlign: 'center' }}>
+          <h1 style={{ fontSize: '1.5rem', fontWeight: 'bold', margin: '10px 0' }}>AI Best Practices</h1>
+        </main>
+      </>
+    )
+}
+
+export default BestPractices;
